e4ae9528968bf330b382e9c237617039f00edd35
Francis Leo <drypasta34@gmail.com>
2025-09-14 22:54:34 +0800

    Update Day22
    feat(day_22):  Input number, calculate factorial.

---
+++ 90-day beginner project/Day 1 to 10/day_22.py
@@ -0,0 +1,7 @@
+import math
+
+
+number_to_factor = int(input("Enter number to factor: "))
+result = math.factorial(number_to_factor)
+
+print(f"Number entered {number_to_factor} and the result of factorial is: {result}")
